af5ab17f5ce9254bfeb96a9c1456b86a18505375
Ouissem <ouissem.redjemi@gmail.com>
2022-02-22 08:48:43 +0100

    Initial Commit

---
+++ Recette/RecetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecetteApp.swift
+//  Recette
+//
+//  Created by m1 on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

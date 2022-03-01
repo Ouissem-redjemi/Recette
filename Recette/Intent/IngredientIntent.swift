2266ead31150cda9f0cc67daf8174a76054c16b9
souhaila kesbi <souhailakesbi@gmail.com>
2022-03-01 16:52:55 +0100

    Intent ingredient

---
+++ Recette/Intent/IngredientIntent.swift
@@ -0,0 +1,83 @@
+//
+//  IngredientIntent.swift
+//  Recette
+//
+//  Created by Souhaila kesbi on 22/02/2022.
+//
+
+import Foundation
+import Combine
+
+enum IngredientIntentState : CustomStringConvertible{
+ 
+    case ready
+    case libelleChanging(String)
+    case allergeneChanging(Allergene)
+    case categorieChanging(CategorieIngredient)
+    case codeChanging(Int)
+    case prixUnitaireChanging(Double)
+    case uniteChanging(String)
+    case listUpdated
+
+
+    var description: String{
+       switch self{
+        case .ready: return "state: .ready"
+        case .libelleChanging(let libelle): return "state: .libelleChanging(\(libelle))"
+        case .allergeneChanging(let allergene): return "state: .allergeneChanging(\(allergene))"
+        case .categorieChanging(let categorie): return "state: .categorieChanging(\(categorie))"
+        case .codeChanging(let code): return "state: .codeChanging(\(code))"
+        case .prixUnitaireChanging(let prix_unitaire): return "state: .prixUnitaireChanging(\(prix_unitaire))"
+       case .uniteChanging(let unite): return "state: .prixUnitaireChanging(\(unite))"
+        case .listUpdated: return  "state: .listUpdated"
+       }
+    }
+}
+
+struct IngredientIntent{
+ 
+   private var state = PassthroughSubject<IngredientIntentState,Never>()
+   
+  
+   
+   func addObserver(viewModel: IngredientViewModel){
+      self.state.subscribe(viewModel)
+   }
+   func addObserver(viewModel: ListeIngredientViewModel){
+       self.state.subscribe(viewModel)
+    }
+   
+   func intentToChange(libelle: String){
+      self.state.send(.libelleChanging(libelle))
+       self.state.send(.listUpdated)
+   }
+    
+    func intentToChange(allergene: Allergene){
+        self.state.send(.allergeneChanging(allergene))
+        self.state.send(.listUpdated)
+    }
+   
+    func intentToChange(categorie: CategorieIngredient){
+        self.state.send(.categorieChanging(categorie))
+        self.state.send(.listUpdated)
+    }
+    
+    func intentToChange(code: Int){
+        self.state.send(.codeChanging(code))
+        self.state.send(.listUpdated)
+    }
+    
+    func intentToChange(prix_unitaire: Double){
+        self.state.send(.prixUnitaireChanging(prix_unitaire))
+        self.state.send(.listUpdated)
+    }
+  
+    func intentToChange(unite: String){
+        self.state.send(.uniteChanging(unite))
+        self.state.send(.listUpdated)
+    }
+    
+    
+    
+  
+}
